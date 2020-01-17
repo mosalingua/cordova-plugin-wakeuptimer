8ba93d937ff0117ce3a8156ee56e659067f54080
Paulo Cristo <uareurapid@users.noreply.github.com>
2020-01-17 11:50:39 +0000

    Update CDVLocalNotification (deprecated) use CDVPluginHandleOpenURLNotification

--- src/ios/WakeupPlugin.m
+++ src/ios/WakeupPlugin.m
@@ -48,7 +48,7 @@ static NSString * const kWakeupPluginAlarmSettingsFile = @"alarmsettings.plist";
 - (void)pluginInitialize
 {
     // watch for local notification
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(wup_onLocalNotification:) name:CDVLocalNotification object:nil]; // if app is in foreground
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(wup_onLocalNotification:) name:CDVPluginHandleOpenURLNotification object:nil]; // if app is in foreground
     
     [UIDevice currentDevice].batteryMonitoringEnabled=YES; // required to determine if device is charging
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(wup_onBatteryStateDidChange:) name:UIDeviceBatteryStateDidChangeNotification object:nil];
@@ -448,7 +448,7 @@ static NSString * const kWakeupPluginAlarmSettingsFile = @"alarmsettings.plist";
 
 - (void)dispose {
     NSLog(@"Wakeup Plugin disposing");
-    [[NSNotificationCenter defaultCenter] removeObserver:self name:CDVLocalNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:CDVPluginHandleOpenURLNotification object:nil];
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UIDeviceBatteryStateDidChangeNotification object:nil];
     [super dispose];
 }
